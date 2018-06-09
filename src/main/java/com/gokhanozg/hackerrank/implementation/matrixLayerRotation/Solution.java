ad3a19bdf30bc6bb963c2a25460491ea11cf155a
gokhanozg <gokhan.ozgozen@gmail.com>
2018-06-09 22:25:26 +0300

    Working on rotation count.

--- src/main/java/com/gokhanozg/hackerrank/implementation/matrixLayerRotation/Solution.java
+++ src/main/java/com/gokhanozg/hackerrank/implementation/matrixLayerRotation/Solution.java
@@ -13,16 +13,65 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     // Complete the matrixRotation function below.
-    static void matrixRotation(int[][] matrix) {
-
-
+    static void matrixRotation(int[][] matrix, int r) {
+        for (int i = 0; i < r; i++) {
+            rotateMatrix(matrix);
+        }
+        printMatrix(matrix);
     }
+
+    private static void rotateMatrix(int[][] matrix) {
+        final int n = matrix[0].length;
+        final int m = matrix.length;
+        int minmn = Math.min(m, n);
+        int rectangleCount = minmn / 2;
+        for (int i = 0; i < rectangleCount; i++) {
+            int leftmost = matrix[i][i];
+            for (int j = i; j < n - i; j++) {
+                if (!((j + 1) == (n - i))) {
+                    matrix[i][j] = matrix[i][j + 1];
+                }
+            }
+            for (int j = i; j < m - i; j++) {
+                if (!((j + 1) == (n - i))) {
+                    matrix[j][n - 1 - i] = matrix[j + 1][n - 1 - i];
+                }
+            }
+            for (int j = n - 1 - i; j > i; j--) {
+                if (!(j - 1 == i - 1)) {
+                    matrix[m - 1 - i][j] = matrix[m - 1 - i][j - 1];
+                }
+            }
+            for (int j = m - 1 - i; j > i; j--) {
+                if (!(j - 1 == i)) {
+                    matrix[j][i] = matrix[j - 1][i];
+                }
+            }
+            matrix[i + 1][i] = leftmost;
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                sb.append(matrix[i][j]);
+                if (j == 3) {
+                    sb.append(System.getProperty("line.separator"));
+                } else {
+                    sb.append(" ");
+                }
+
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
 
     public static void main(String[] args) {
         String[] mnr = scanner.nextLine().split(" ");
 
         int m = Integer.parseInt(mnr[0]);
-
         int n = Integer.parseInt(mnr[1]);
 
         int r = Integer.parseInt(mnr[2]);
@@ -39,7 +88,7 @@ public class Solution {
             }
         }
 
-        matrixRotation(matrix);
+        matrixRotation(matrix, r);
 
         scanner.close();
     }
---
+++ src/main/java/com/gokhanozg/hackerrank/implementation/matrixLayerRotation/TestSol.java
@@ -0,0 +1,12 @@
+package com.gokhanozg.hackerrank.implementation.matrixLayerRotation;
+
+public class TestSol {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("afwawffwa");
+        sb.append("\n");
+        sb.append("afwfwawfafwafwa");
+        System.out.println(sb.toString());
+    }
+}
