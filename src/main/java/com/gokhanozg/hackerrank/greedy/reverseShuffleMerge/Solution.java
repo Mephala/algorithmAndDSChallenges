d93adb67fedd738fbb161e4a73467cbcfba8319f
Mephala <gokhan.ozgozen@gmail.com>
2018-06-10 05:21:32 +0300

    https://www.hackerrank.com/challenges/reverse-shuffle-merge/problem

---
+++ src/main/java/com/gokhanozg/hackerrank/greedy/reverseShuffleMerge/Solution.java
@@ -0,0 +1,35 @@
+package com.gokhanozg.hackerrank.greedy.reverseShuffleMerge;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    // Complete the reverseShuffleMerge function below.
+    static String reverseShuffleMerge(String s) {
+
+        return "asdfa";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        String result = reverseShuffleMerge(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
