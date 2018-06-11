96575dec0485aed54cc389ff99dbfe4627ebd196
Mephala <gokhan.ozgozen@gmail.com>
2018-06-12 01:53:54 +0300

    countDiv

--- src/main/java/com/gokhanozg/codility/prefixSums/countDiv/Solution.java
+++ src/main/java/com/gokhanozg/codility/prefixSums/countDiv/Solution.java
@@ -2,6 +2,31 @@ package com.gokhanozg.codility.prefixSums.countDiv;
 
 class Solution {
     public int solution(int A, int B, int K) {
-        // write your code in Java SE 8
+        int modA = A % K;
+        int modB = B % K;
+        int start = 0;
+        int end = 0;
+        if (modA == 0) {
+            start = A / K;
+        } else if (modA != 0) {
+            int tmp = A + (K - modA);
+            if (tmp > B) {
+                return 0;
+            } else {
+                start = tmp / K;
+            }
+        }
+        if (modB == 0) {
+            end = B / K;
+        } else if (modB != 0) {
+            int tmp = B - modB;
+            if (tmp < A) {
+                return 0;
+            } else {
+                end = tmp / K;
+            }
+        }
+        return end - start + 1;
+
     }
 }
--- src/main/java/com/gokhanozg/codility/prefixSums/countDiv/TestSol.java
+++ src/main/java/com/gokhanozg/codility/prefixSums/countDiv/TestSol.java
@@ -1,4 +1,16 @@
 package com.gokhanozg.codility.prefixSums.countDiv;
 
 public class TestSol {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(6, 11, 2));
+        System.out.println(new Solution().solution(0, 0, 1));
+        System.out.println(new Solution().solution(5, 5, 5));
+        System.out.println(new Solution().solution(2, 6, 2));
+        System.out.println(new Solution().solution(1, 6, 2));
+        System.out.println(new Solution().solution(0, 6, 2));
+        System.out.println(new Solution().solution(11, 345, 17));
+        System.out.println(new Solution().solution(30, 50, 1));
+        System.out.println(new Solution().solution(0, 2, 1));
+    }
 }
