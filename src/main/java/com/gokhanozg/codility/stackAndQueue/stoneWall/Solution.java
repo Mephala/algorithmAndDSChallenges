c0b26a8e6070011bc4aefb24178aa450f42c9aae
Mephala <gokhan.ozgozen@gmail.com>
2018-06-15 05:52:41 +0300

    Very bad performance solution : )

--- src/main/java/com/gokhanozg/codility/stackAndQueue/stoneWall/Solution.java
+++ src/main/java/com/gokhanozg/codility/stackAndQueue/stoneWall/Solution.java
@@ -1,8 +1,47 @@
 package com.gokhanozg.codility.stackAndQueue.stoneWall;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+
+    int totalBlocks = 0;
+
     public int solution(int[] H) {
-        // write your code in Java SE 8
-        return 9;
+        if (H == null || H.length == 0) {
+            return 0;
+        } else if (H.length == 1) {
+            totalBlocks++;
+        } else {
+            int lowest = Integer.MAX_VALUE;
+            for (int i = 0; i < H.length; i++) {
+                if (H[i] < lowest) {
+                    lowest = H[i];
+                }
+            }
+            totalBlocks++;
+            List<Integer> subBlock = new ArrayList<>();
+            for (int i = 0; i < H.length; i++) {
+                if (H[i] == lowest) {
+                    int[] subArr = new int[subBlock.size()];
+                    for (int j = 0; j < subBlock.size(); j++) {
+                        subArr[j] = subBlock.get(j);
+                    }
+                    solution(subArr);
+                    subBlock.clear();
+                } else {
+                    subBlock.add(H[i] - lowest);
+                }
+            }
+            if (subBlock.size() > 0) {
+                int[] subArr = new int[subBlock.size()];
+                for (int j = 0; j < subBlock.size(); j++) {
+                    subArr[j] = subBlock.get(j);
+                }
+                solution(subArr);
+                subBlock.clear();
+            }
+        }
+        return totalBlocks;
     }
 }
--- src/main/java/com/gokhanozg/codility/stackAndQueue/stoneWall/TestSol.java
+++ src/main/java/com/gokhanozg/codility/stackAndQueue/stoneWall/TestSol.java
@@ -1,4 +1,15 @@
 package com.gokhanozg.codility.stackAndQueue.stoneWall;
 
 public class TestSol {
+
+    public static void main(String[] args) {
+//        PriorityQueue<Integer> pq = new PriorityQueue<>();
+//        pq.add(3);
+//        pq.add(3);
+//        System.out.println(pq.size());
+        System.out.println(new Solution().solution(new int[]{1}));
+        System.out.println(new Solution().solution(new int[]{8, 8, 8, 8, 8, 8, 8, 8, 8}));
+        System.out.println(new Solution().solution(new int[]{8, 8, 5, 7, 9, 8, 7, 4, 8}));
+        System.out.println(new Solution().solution(new int[]{4, 4, 5, 5, 6, 6, 7, 7}));
+    }
 }
