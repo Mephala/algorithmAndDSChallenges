0fe02faeaab878330b3fa638b0e66f649a8447ae
Mephala <gokhan.ozgozen@gmail.com>
2018-06-15 05:01:55 +0300

    Working on Stone Wall.

--- src/main/java/com/gokhanozg/codility/stackAndQueue/stoneWall/Solution.java
+++ src/main/java/com/gokhanozg/codility/stackAndQueue/stoneWall/Solution.java
@@ -3,5 +3,6 @@ package com.gokhanozg.codility.stackAndQueue.stoneWall;
 class Solution {
     public int solution(int[] H) {
         // write your code in Java SE 8
+        return 9;
     }
 }
--- src/main/java/com/gokhanozg/topcoder/practice/AB/AB.java
+++ src/main/java/com/gokhanozg/topcoder/practice/AB/AB.java
@@ -3,6 +3,7 @@ package com.gokhanozg.topcoder.practice.AB;
 public class AB {
 
     public String CreateString(int N, int K) {
-
+        return "asdas";
+        //TODO Solve this.
     }
 }
