fbe7ab2dbbdda21ce2ccdf98bdf96c9c8adc9bb4
Mephala <gokhan.ozgozen@gmail.com>
2018-06-15 10:06:36 +0300

    Very bad performance solution : )

---
+++ src/main/java/com/gokhanozg/codility/stackAndQueue/stoneWall/Solution2.java
@@ -0,0 +1,21 @@
+package com.gokhanozg.codility.stackAndQueue.stoneWall;
+
+
+import java.util.Stack;
+
+//Solution with stack
+public class Solution2 {
+
+    public int solution(int[] H) {
+//        Stack<Integer> stack = new Stack<>();
+//        int blocks = 0;
+//        for (int i : H) {
+//            if(stack.size() == 0){
+//                stack.push(i);
+//            }else{
+//                if
+//            }
+//        }
+        return 8;
+    }
+}
