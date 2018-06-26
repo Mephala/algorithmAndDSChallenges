d937efe2c82e5310626043267fa83d5023505d96
Mephala <gokhan.ozgozen@gmail.com>
2018-06-26 23:06:30 +0300

    minuteToWinIt solution. (in progress, failing 3,10,11 )

--- src/main/java/com/gokhanozg/hackerrank/contests/w38/minuteToWinIt/Solution.java
+++ src/main/java/com/gokhanozg/hackerrank/contests/w38/minuteToWinIt/Solution.java
@@ -12,54 +12,14 @@ public class Solution {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+
     // Complete the minuteToWinIt function below.
     static int minuteToWinIt(int[] a, int k) {
-        int[] lrc = new int[a.length];
-        int[] rlc = new int[a.length];
-        for (int i = 0; i < a.length; i++) {
-            lrc[i] = a[i];
-            rlc[i] = a[i];
-        }
-        int lr = lrMinCount(lrc, k);
-        int rl = rlMinCount(rlc, k);
-        int[] difArr = new int[a.length - 1];
-        for (int i = 0; i < a.length - 1; i++) {
-            difArr[i] = a[i + 1] - a[i];
-        }
-        int diffCount = 0;
-        for (int i = 0; i < difArr.length; i++) {
-            if (difArr[i] != k) {
-                diffCount++;
-            }
-        }
 
-
-//        return Math.min(rl, Math.min(mid, lr));
         return 8;
 
     }
 
-    private static int rlMinCount(int[] rlc, int k) {
-        int rl = 0;
-        for (int i = rlc.length - 1; i > 0; i--) {
-            if (rlc[i] - rlc[i - 1] != k) {
-                rl++;
-                rlc[i - 1] = rlc[i] - k;
-            }
-        }
-        return rl;
-    }
-
-    private static int lrMinCount(int[] lrc, int k) {
-        int lr = 0;
-        for (int i = 0; i < lrc.length - 1; i++) {
-            if (lrc[i + 1] - lrc[i] != k) {
-                lr++;
-                lrc[i + 1] = lrc[i] + k;
-            }
-        }
-        return lr;
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
