7bf6fe0755345be404555bce6aef423cb79d5d42
Mephala <gokhan.ozgozen@gmail.com>
2018-07-05 07:32:52 +0300

    fish solution.

--- src/main/java/com/gokhanozg/codility/stackAndQueue/fish/Solution.java
+++ src/main/java/com/gokhanozg/codility/stackAndQueue/fish/Solution.java
@@ -1,16 +1,20 @@
 package com.gokhanozg.codility.stackAndQueue.fish;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 class Solution {
     public int solution(int[] A, int[] B) {
         Stack<Integer> upstreamStack = new Stack<>();
         Stack<Integer> downStreamStack = new Stack<>();
+        Map<Integer, Integer> fishSizeToPosition = new HashMap<>();
         int ignoredFishCount = 0;
         // 0 = upstream
         // 1 = downstream
         boolean downStreamDetected = false;
         for (int i = 0; i < A.length; i++) {
+            fishSizeToPosition.put(A[i], i);
             if (!downStreamDetected && B[i] == 0) {
                 ignoredFishCount++; //this fish won't encounter any other fish.
             } else if (!downStreamDetected && B[i] == 1) {
@@ -18,22 +22,27 @@ class Solution {
                 downStreamStack.push(A[i]);
             } else if (downStreamDetected && B[i] == 1) {
                 downStreamStack.push(A[i]);
-                removeSmallerFish(upstreamStack, downStreamStack);
+                removeSmallerFish(upstreamStack, downStreamStack, fishSizeToPosition);
             } else {
                 // downStreamDetected and fish is moving upstream
                 upstreamStack.push(A[i]);
-                removeSmallerFish(upstreamStack, downStreamStack);
+                removeSmallerFish(upstreamStack, downStreamStack, fishSizeToPosition);
             }
         }
         return upstreamStack.size() + downStreamStack.size() + ignoredFishCount;
     }
 
-    private void removeSmallerFish(Stack<Integer> upstreamStack, Stack<Integer> downStreamStack) {
-        while (!upstreamStack.empty() && !downStreamStack.empty()) {
-            if (upstreamStack.peek() > downStreamStack.peek()) {
+    private void removeSmallerFish(Stack<Integer> upstreamStack, Stack<Integer> downStreamStack, Map<Integer, Integer> fishSizeToPosition) {
+        boolean encounter = !upstreamStack.empty() && !downStreamStack.empty();
+        while (encounter) {
+            if (upstreamStack.peek() > downStreamStack.peek() && fishSizeToPosition.get(upstreamStack.peek()) > fishSizeToPosition.get(downStreamStack.peek())) {
                 downStreamStack.pop();
-            } else {
+                encounter = !upstreamStack.empty() && !downStreamStack.empty();
+            } else if (upstreamStack.peek() < downStreamStack.peek() && fishSizeToPosition.get(upstreamStack.peek()) > fishSizeToPosition.get(downStreamStack.peek())) {
                 upstreamStack.pop();
+                encounter = !upstreamStack.empty() && !downStreamStack.empty();
+            } else {
+                encounter = false;
             }
         }
     }
--- src/main/java/com/gokhanozg/codility/stackAndQueue/fish/TestSol.java
+++ src/main/java/com/gokhanozg/codility/stackAndQueue/fish/TestSol.java
@@ -6,12 +6,13 @@ package com.gokhanozg.codility.stackAndQueue.fish;
 public class TestSol {
 
     public static void main(String[] args) {
-//        System.out.println("2," + new Solution().solution(new int[]{4, 3, 2, 1, 5}, new int[]{0, 1, 0, 0, 0}));
-//        System.out.println("1," + new Solution().solution(new int[]{8}, new int[]{1}));
-//        System.out.println("1," + new Solution().solution(new int[]{8}, new int[]{0}));
+        System.out.println("2," + new Solution().solution(new int[]{4, 3, 2, 1, 5}, new int[]{0, 1, 0, 0, 0}));
+        System.out.println("1," + new Solution().solution(new int[]{8}, new int[]{1}));
+        System.out.println("1," + new Solution().solution(new int[]{8}, new int[]{0}));
         System.out.println("2," + new Solution().solution(new int[]{4, 3, 2, 1, 5, 7}, new int[]{1, 1, 1, 0, 0, 0}));
         System.out.println("6," + new Solution().solution(new int[]{4, 3, 2, 1, 5, 7}, new int[]{1, 1, 1, 1, 1, 1}));
         System.out.println("6," + new Solution().solution(new int[]{4, 3, 2, 1, 5, 7}, new int[]{0, 0, 0, 0, 0, 0}));
         System.out.println("5," + new Solution().solution(new int[]{4, 3, 2, 1, 5, 7}, new int[]{0, 0, 0, 0, 1, 0}));
+        System.out.println("4," + new Solution().solution(new int[]{8, 5, 9, 6, 7, 78}, new int[]{0, 1, 0, 1, 0, 1}));
     }
 }
