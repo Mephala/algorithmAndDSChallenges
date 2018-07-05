76ffc95e227acf590e5e864745d1480b4cc8b629
Mephala <gokhan.ozgozen@gmail.com>
2018-07-05 07:59:30 +0300

    nesting complete.

---
+++ src/main/java/com/gokhanozg/codility/stackAndQueue/nesting/TestSol.java
@@ -0,0 +1,15 @@
+package com.gokhanozg.codility.stackAndQueue.nesting;
+
+/**
+ * Created by Mephalay on 7/5/18.
+ */
+public class TestSol {
+
+    public static void main(String[] args) {
+        System.out.println("1," + new Solution().solution("()"));
+        System.out.println("1," + new Solution().solution(""));
+        System.out.println("1," + new Solution().solution("((((())))()()()()()()()()()()())"));
+        System.out.println("0," + new Solution().solution("(((())))()()()()()()()()()()())"));
+
+    }
+}
