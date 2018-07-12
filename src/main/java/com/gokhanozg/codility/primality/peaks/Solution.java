1287b3c4ad2a5ed60f92faf1e0adc5af679a160f
Mephala <gokhan.ozgozen@gmail.com>
2018-07-12 03:16:04 +0300

    minPerimeterRectangle.

---
+++ src/main/java/com/gokhanozg/codility/primality/peaks/Solution.java
@@ -0,0 +1,8 @@
+package com.gokhanozg.codility.primality.peaks;
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        return 2;
+    }
+}
---
+++ src/main/java/com/gokhanozg/codility/primality/peaks/TestSol.java
@@ -0,0 +1,11 @@
+package com.gokhanozg.codility.primality.peaks;
+
+/**
+ * Created by Mephalay on 12-Jul-18.
+ */
+public class TestSol {
+
+    public static void main(String[] args) {
+
+    }
+}
