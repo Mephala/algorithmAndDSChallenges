dd5c0cf5eab038246b84e469c3f0ec00c1879d20
Mephala <gokhan.ozgozen@gmail.com>
2018-07-17 03:25:19 +0300

    code wars.

---
+++ src/main/java/com/gokhanozg/codeWars/smallestInteger/SmallestIntegerFinder.java
@@ -0,0 +1,10 @@
+package com.gokhanozg.codeWars.smallestInteger;
+
+import java.util.stream.IntStream;
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+        return IntStream.of(args).min().getAsInt();
+
+    }
+}
