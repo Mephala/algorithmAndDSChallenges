3cb2a188c230ce30ab8c5035bfb1db2b635df21d
Mephala <gokhan.ozgozen@gmail.com>
2018-07-19 16:16:48 +0300

    minAbsSum

---
+++ src/main/java/com/gokhanozg/codility/dynamicProgramming/minAbs/Solution.java
@@ -0,0 +1,39 @@
+package com.gokhanozg.codility.dynamicProgramming.minAbs;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] A) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        long sum1 = 0L;
+        long sum2 = 0L;
+        for (int i = 0; i < A.length; i++) {
+            maxHeap.add(Math.abs(A[i]));
+            sum1 += Math.abs(A[i]);
+        }
+        if (maxHeap.size() == 0) {
+            return 0;
+        }
+        long dif = sum1 - sum2;
+        while (dif >= (maxHeap.peek() * 2)) {
+            if (maxHeap.size() != 0) {
+                long val = maxHeap.poll();
+                sum1 -= val;
+                sum2 += val;
+                dif = sum1 - sum2;
+                dif = Math.abs(dif);
+                if (maxHeap.size() == 0) {
+                    break;
+                }
+            }
+        }
+        while (maxHeap.size() > 0) {
+            long val = maxHeap.poll();
+            if (dif >= val * 2) {
+                dif -= val * 2;
+            }
+        }
+        return (int) dif;
+    }
+}
---
+++ src/main/java/com/gokhanozg/codility/dynamicProgramming/minAbs/TestSol.java
@@ -0,0 +1,20 @@
+package com.gokhanozg.codility.dynamicProgramming.minAbs;
+
+/**
+ * Created by Mephalay on 19-Jul-18.
+ */
+public class TestSol {
+    public static void main(String[] args) {
+        System.out.println("0," + new Solution().solution(new int[]{3, 2, 2, 1}));
+        System.out.println("0," + new Solution().solution(new int[]{1, 2, 5, 8}));
+        System.out.println("0," + new Solution().solution(new int[]{1, -2, 5, 8}));
+        System.out.println("0," + new Solution().solution(new int[]{1, 5, -2, 2}));
+        System.out.println("0," + new Solution().solution(new int[]{2, 2, 3, 3, 8, 2, 2}));
+        System.out.println("2," + new Solution().solution(new int[]{4, 9, 1, 5, 20, 14, 6, 7}));
+        System.out.println("0," + new Solution().solution(new int[]{}));
+        System.out.println("6," + new Solution().solution(new int[]{6}));
+        System.out.println("500," + new Solution().solution(new int[]{100, 700, 100}));
+        System.out.println("0," + new Solution().solution(new int[]{1, 5, -2, 5, 2, 3}));
+        System.out.println("0," + new Solution().solution(new int[]{1, 2, 3, 4, 1, 1}));
+    }
+}
