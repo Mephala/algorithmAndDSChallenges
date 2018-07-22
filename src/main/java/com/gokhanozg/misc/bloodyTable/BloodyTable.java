f086ab446d6146b222f7d3533645699ce022fd93
Mephala <gokhan.ozgozen@gmail.com>
2018-07-22 15:20:33 +0300

    Working on abbreviation.

---
+++ src/main/java/com/gokhanozg/misc/bloodyTable/BloodyTable.java
@@ -0,0 +1,16 @@
+package com.gokhanozg.misc.bloodyTable;
+
+/**
+ * Created by Mephalay on 22-Jul-18.
+ */
+
+/**
+ * Around a round table, n number of people are dining. 1 person kills the one at his right, and passes the knife to
+ * the second person on his right. This goes on until there is only a single survivor.
+ */
+public class BloodyTable {
+
+    public static void main(String[] args) {
+
+    }
+}
