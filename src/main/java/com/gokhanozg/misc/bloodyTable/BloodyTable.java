d639c3e7b4954c1424cee8ac4c8b1ba9ee3f563e
Mephala <gokhan.ozgozen@gmail.com>
2018-07-25 15:44:41 +0300

    Working on abbreviation.

--- src/main/java/com/gokhanozg/misc/bloodyTable/BloodyTable.java
+++ src/main/java/com/gokhanozg/misc/bloodyTable/BloodyTable.java
@@ -4,13 +4,34 @@ package com.gokhanozg.misc.bloodyTable;
  * Created by Mephalay on 22-Jul-18.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Around a round table, n number of people are dining. 1 person kills the one at his right, and passes the knife to
  * the second person on his right. This goes on until there is only a single survivor.
  */
 public class BloodyTable {
 
-    public static void main(String[] args) {
-
+    public static int run(int totalPeople) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= totalPeople; i++) {
+            list.add(i);
+        }
+        int index = 0;
+        while (list.size() != 1) {
+            if (index + 1 == list.size()) {
+                //we are at last element
+                list.remove(0);
+                index = 0;
+            } else if (index + 1 > list.size()) {
+                list.remove(1);
+                index = 1;
+            } else {
+                list.remove(index + 1);
+                index++;
+            }
+        }
+        return list.get(0);
     }
 }
---
+++ src/main/java/com/gokhanozg/misc/bloodyTable/TestSol.java
@@ -0,0 +1,19 @@
+package com.gokhanozg.misc.bloodyTable;
+
+/**
+ * Created by Mephalay on 22-Jul-18.
+ */
+public class TestSol {
+
+    public static void main(String[] args) {
+        System.out.println(BloodyTable.run(1));
+        System.out.println(BloodyTable.run(2));
+        System.out.println(BloodyTable.run(3));
+        System.out.println(BloodyTable.run(4));
+        System.out.println(BloodyTable.run(5));
+        System.out.println(BloodyTable.run(6));
+        System.out.println(BloodyTable.run(7));
+        System.out.println(BloodyTable.run(1000));
+        System.out.println(BloodyTable.run(100));
+    }
+}
