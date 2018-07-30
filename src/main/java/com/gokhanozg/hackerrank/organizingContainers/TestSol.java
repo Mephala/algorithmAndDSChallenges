40cf763cac1fe978ca9b46482b1c31faf7234374
Mephala <gokhan.ozgozen@gmail.com>
2018-07-30 22:51:19 +0300

    organizing containers

---
+++ src/main/java/com/gokhanozg/hackerrank/organizingContainers/TestSol.java
@@ -0,0 +1,14 @@
+package com.gokhanozg.hackerrank.organizingContainers;
+
+/**
+ * Created by Gokhan Ozgozen on 30-Jul-18.
+ */
+public class TestSol {
+
+    public static void main(String[] args) {
+//        System.out.println("Possible," + Solution.organizingContainers(new int[][]{{1,1},{1,1}}));
+//        System.out.println("Impossible," + Solution.organizingContainers(new int[][]{{0,2},{1,1}}));
+        System.out.println("Possible," + Solution.organizingContainers(new int[][]{{0, 4, 0}, {2, 0, 2}, {2, 0, 0}}));
+//        System.out.println("Possible," + Solution.organizingContainers(new int[][]{{997612619, 934920795, 998879231, 999926463},{960369681, 997828120, 999792735, 979622676},{999013654, 998634077, 997988323, 958769423},{997409523, 999301350, 940952923, 993020546}}));
+    }
+}
