245932612d6e328c044bfd19c3f79152a552a3d5
Mephala <gokhan.ozgozen@gmail.com>
2018-08-01 23:34:02 +0300

    solving random hackerrank.

---
+++ src/main/java/com/gokhanozg/hackerrank/lisasWorkbook/Solution.java
@@ -0,0 +1,46 @@
+package com.gokhanozg.hackerrank.lisasWorkbook;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    // Complete the workbook function below.
+    static int workbook(int n, int k, int[] arr) {
+
+        return 5;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = workbook(n, k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
