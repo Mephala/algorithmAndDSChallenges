44fbe3dd2fae747ee36e80c811b1f1989c832961
Mephala <gokhan.ozgozen@gmail.com>
2018-08-04 19:12:29 +0300

    solving random hackerrank.

---
+++ src/main/java/com/gokhanozg/hackerrank/threeDSurfaceArea/Solution.java
@@ -0,0 +1,48 @@
+package com.gokhanozg.hackerrank.threeDSurfaceArea;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    // Complete the surfaceArea function below.
+    static int surfaceArea(int[][] A) {
+
+        return 3;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] HW = scanner.nextLine().split(" ");
+
+        int H = Integer.parseInt(HW[0]);
+
+        int W = Integer.parseInt(HW[1]);
+
+        int[][] A = new int[H][W];
+
+        for (int i = 0; i < H; i++) {
+            String[] ARowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < W; j++) {
+                int AItem = Integer.parseInt(ARowItems[j]);
+                A[i][j] = AItem;
+            }
+        }
+
+        int result = surfaceArea(A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
