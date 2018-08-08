1b1d648ba3947148d54edf50aafd991184b55f8e
Mephala <gokhan.ozgozen@gmail.com>
2018-08-08 21:08:41 +0300

    solving random hackerrank.

--- src/main/java/com/gokhanozg/hackerrank/threeDSurfaceArea/Solution.java
+++ src/main/java/com/gokhanozg/hackerrank/threeDSurfaceArea/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Solution {
 
@@ -11,8 +12,65 @@ public class Solution {
 
     // Complete the surfaceArea function below.
     static int surfaceArea(int[][] A) {
+        Stack<Integer> countingStack = new Stack<>();
+        int count = 0;
+        //left to right
+        for (int i = 0; i < A.length; i++) {
+            countingStack.push(0);
+            for (int j = 0; j < A[0].length; j++) {
+                int val = A[i][j];
+                if (val > countingStack.peek()) {
+                    count += val - countingStack.peek();
+                }
+                countingStack.push(val);
+            }
+        }
+        //right to left
+        for (int i = 0; i < A.length; i++) {
+            countingStack.push(0);
+            for (int j = A[0].length - 1; j >= 0; j--) {
+                int val = A[i][j];
+                if (val > countingStack.peek()) {
+                    count += val - countingStack.peek();
+                }
+                countingStack.push(val);
+            }
+        }
+        //front to back
+        for (int i = 0; i < A[0].length; i++) {
+            countingStack.push(0);
+            for (int j = 0; j < A.length; j++) {
+                int val = A[j][i];
+                if (val > countingStack.peek()) {
+                    count += val - countingStack.peek();
+                }
+                countingStack.push(val);
+            }
+        }
+        //back to front
+        for (int i = 0; i < A[0].length; i++) {
+            countingStack.push(0);
+            for (int j = A.length - 1; j >= 0; j--) {
+                int val = A[j][i];
+                if (val > countingStack.peek()) {
+                    count += val - countingStack.peek();
+                }
+                countingStack.push(val);
+            }
+        }
+        int nonZeroVals = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                int val = A[i][j];
+                if (val > 0) {
+                    nonZeroVals++;
+                }
+            }
+        }
+        count += 2 * nonZeroVals; // from top and bottom should be equal.
 
-        return 3;
+        return count;
+
     }
 
     public static void main(String[] args) throws IOException {
---
+++ src/main/java/com/gokhanozg/hackerrank/threeDSurfaceArea/TestSol.java
@@ -0,0 +1,13 @@
+package com.gokhanozg.hackerrank.threeDSurfaceArea;
+
+/**
+ * Created by Gokhan Ozgozen on 08-Aug-18.
+ */
+public class TestSol {
+
+    public static void main(String[] args) {
+//        System.out.println(Solution.surfaceArea(new int[][]{{1}}));
+//        System.out.println(Solution.surfaceArea(new int[][]{{1,3,4},{2,2,3},{1,2,4}}));
+        System.out.println(Solution.surfaceArea(new int[][]{{1}, {2}, {3}}));
+    }
+}
