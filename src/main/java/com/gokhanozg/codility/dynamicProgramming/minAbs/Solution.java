1bedbbc447d9e08ec1c3ed96b79771297ccc9dc7
Mephala <gokhan.ozgozen@gmail.com>
2018-08-11 22:50:16 +0300

    codility exercies.

--- src/main/java/com/gokhanozg/codility/dynamicProgramming/minAbs/Solution.java
+++ src/main/java/com/gokhanozg/codility/dynamicProgramming/minAbs/Solution.java
@@ -1,43 +1,30 @@
 package com.gokhanozg.codility.dynamicProgramming.minAbs;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 class Solution {
     //Fails on one case, achieves almost full score. Don't know yet why it fails. I will update once I have my curiosity
     // is back on this : )
 
     public int solution(int[] A) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        long sum1 = 0L;
-        long sum2 = 0L;
-        for (int i = 0; i < A.length; i++) {
-            maxHeap.add(Math.abs(A[i]));
-            sum1 += Math.abs(A[i]);
+        int sum = 0;
+        for (int i : A) {
+            sum += Math.abs(i);
         }
-        if (maxHeap.size() == 0) {
-            return 0;
-        }
-        long dif = sum1 - sum2;
-        while (dif >= (maxHeap.peek() * 2)) {
-            if (maxHeap.size() != 0) {
-                long val = maxHeap.poll();
-                sum1 -= val;
-                sum2 += val;
-                dif = sum1 - sum2;
-                dif = Math.abs(dif);
-                if (maxHeap.size() == 0) {
-                    break;
+        int half = sum / 2;
+        int[] sums = new int[sum + 1];
+        sums[0] = 1;
+        for (int i = 0; i < sums.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (sums[i] == 1 && (i + A[j]) <= sum) {
+                    sums[i + A[j]] = 1;
                 }
             }
         }
-        while (maxHeap.size() > 0) {
-            long val = maxHeap.poll();
-            if (dif >= val * 2) {
-                dif -= val * 2;
+        for (int i = half; i >= 0; i--) {
+            if (sums[i] == 1) {
+                return sum - i;
             }
         }
-        return (int) dif;
+        return -1;
     }
 
 
