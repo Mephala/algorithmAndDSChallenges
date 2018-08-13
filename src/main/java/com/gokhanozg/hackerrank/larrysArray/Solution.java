cb819021e3611706978b507fbf0828e94673a35e
Mephala <gokhan.ozgozen@gmail.com>
2018-08-13 20:24:22 +0300

    random hackerrank.

--- src/main/java/com/gokhanozg/hackerrank/larrysArray/Solution.java
+++ src/main/java/com/gokhanozg/hackerrank/larrysArray/Solution.java
@@ -3,48 +3,26 @@ package com.gokhanozg.hackerrank.larrysArray;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
 
     private static final Scanner scanner = new Scanner(System.in);
 
+
     // Complete the larrysArray function below.
     static String larrysArray(int[] A) {
-        int[] sorted = new int[A.length];
+        int inversion = 0;
         for (int i = 0; i < A.length; i++) {
-            sorted[i] = A[i];
-        }
-        Arrays.sort(sorted);
-        for (int i = 0; i < sorted.length - 2; i++) {
-            int[] sortedSlice = Arrays.copyOfRange(sorted, i, i + 3);
-            int[] aSlice = Arrays.copyOfRange(A, i, i + 3);
-            int attempt = 0;
-            while (attempt < 3 && sortedSlice[0] != aSlice[0]) {
-                aSlice = rotate(aSlice);
-                attempt++;
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] > A[j])
+                    inversion++;
             }
-            if (sortedSlice[0] != aSlice[0]) {
-                return "NO";
-            }
-            A[i] = aSlice[0];
-            A[i + 1] = aSlice[1];
-            A[i + 2] = aSlice[2];
         }
-        if (Arrays.equals(sorted, A)) {
-            return "YES";
-        }
-        return "NO";
+        return inversion % 2 == 0 ? "YES" : "NO";
+
     }
 
-    static int[] rotate(int[] a) {
-        int[] ret = new int[3];
-        ret[0] = a[1];
-        ret[1] = a[2];
-        ret[2] = a[0];
-        return ret;
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
--- src/main/java/com/gokhanozg/hackerrank/larrysArray/TestSol.java
+++ src/main/java/com/gokhanozg/hackerrank/larrysArray/TestSol.java
@@ -5,14 +5,13 @@ package com.gokhanozg.hackerrank.larrysArray;
  */
 public class TestSol {
     public static void main(String[] args) {
-        System.out.println("YES," + Solution.larrysArray(new int[]{3, 1, 2}));
-        System.out.println("YES," + Solution.larrysArray(new int[]{1, 3, 4, 2}));
-        System.out.println("NO," + Solution.larrysArray(new int[]{1, 2, 3, 5, 4}));
-        System.out.println("YES," + Solution.larrysArray(new int[]{1, 1, 1, 1}));
-        System.out.println("YES," + Solution.larrysArray(new int[]{1, 1, 1}));
-        System.out.println("NO," + Solution.larrysArray(new int[]{1, 2, 8, 9, 10, 3, 4, 5}));
-        System.out.println("YES," + Solution.larrysArray(new int[]{4, 1, 3, 2}));
-        System.out.println("NO," + Solution.larrysArray(new int[]{9, 6, 8, 12, 3, 7, 1, 11, 10, 2, 5, 4}));
-        System.out.println("YES," + Solution.larrysArray(new int[]{17, 21, 2, 1, 16, 9, 12, 11, 6, 18, 20, 7, 14, 8, 19, 10, 3, 4, 13, 5, 15}));
+        System.out.println("YES," + Solution.larrysArray(new int[]{2, 3, 1}));
+//        System.out.println("YES," + Solution.larrysArray(new int[]{3, 1, 2}));
+//        System.out.println("YES," + Solution.larrysArray(new int[]{1, 3, 4, 2}));
+//        System.out.println("NO," + Solution.larrysArray(new int[]{1, 2, 3, 5, 4}));
+//        System.out.println("NO," + Solution.larrysArray(new int[]{1, 2, 8, 9, 10, 3, 4, 5}));
+//        System.out.println("YES," + Solution.larrysArray(new int[]{4, 1, 3, 2}));
+//        System.out.println("NO," + Solution.larrysArray(new int[]{9, 6, 8, 12, 3, 7, 1, 11, 10, 2, 5, 4}));
+//        System.out.println("YES," + Solution.larrysArray(new int[]{17, 21, 2, 1, 16, 9, 12, 11, 6, 18, 20, 7, 14, 8, 19, 10, 3, 4, 13, 5, 15}));
     }
 }
