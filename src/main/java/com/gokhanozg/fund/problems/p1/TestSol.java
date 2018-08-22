b8dc27cb87b79c7f145057ba447c7fca40265426
Mephala <gokhan.ozgozen@gmail.com>
2018-08-22 16:19:46 +0300

    codility SlalomSkiing. Sucky performance.

--- src/main/java/com/gokhanozg/fund/problems/p1/TestSol.java
+++ src/main/java/com/gokhanozg/fund/problems/p1/TestSol.java
@@ -10,5 +10,7 @@ public class TestSol {
         System.out.println(new Solution().solution(new int[]{3, 2, 1, 6, 5}, new int[]{4, 2, 1, 3, 3}));
         System.out.println(new Solution().solution(new int[]{1, 2}, new int[]{1, 2}));
         System.out.println(new Solution().solution(new int[]{3, 5, 5, 3, 4}, new int[]{5, 5, 5, 5, 5}));
+        System.out.println(new Solution().solution(new int[]{5, 5, 4, 3, 2, 4, 4, 5, 6, 7, 9}, new int[]{5, 5, 4, 3, 2, 4, 4, 5, 6, 7, 9}));
+        System.out.println(new Solution().solution(new int[]{1, 2, 3, 4}, new int[]{1, 2, 4, 5}));
     }
 }
