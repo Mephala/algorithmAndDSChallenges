52ebcb55e36a0a16f07b703c1ffd565f57af98fe
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2018-11-16 18:42:22 +0300

    test

--- src/main/java/com/gokhanozg/custom/Test2.java
+++ src/main/java/com/gokhanozg/custom/Test2.java
@@ -1,7 +1,9 @@
 package com.gokhanozg.custom;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Gokhan Ozgozen on 12-Nov-18.
@@ -15,12 +17,18 @@ public class Test2 {
         t.value = "c";
         map.put(t, "a");
         T t2 = new T();
-        t.id = 2;
-        t.value = "c";
+        t2.id = 3;
+        t2.value = "d";
         map.put(t2, "b");
         System.out.println(map.size());
         System.out.println(map.get(t));
         System.out.println(map.get(t2));
+        Set<T> hashSet = new HashSet<>();
+        hashSet.add(t);
+        hashSet.add(t2);
+        System.out.println(hashSet.size());
+        System.out.println(t.equals(t2));
+
 
     }
 
